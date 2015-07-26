b3aa461a195d6772651e85d8165cd81ae092bb4d
growlf <growlf@oddotter.com>
2015-07-26 15:04:25 -0700

    Cleaning server side code from client - what was I thinking?  *sigh*

--- client/js/configs/app.js
+++ client/js/configs/app.js
@@ -2,9 +2,4 @@ if (Meteor.isClient) {
 
   angular.module('yetibox', ['angular-meteor', 'ui.router']);
 
-  orion.dictionary.addDefinition('description', 'site', {
-      type: String,
-      label: 'Description'
-  });
-  
 }
