5298dd83f83b3be6f7e8de05ec687b0cb27a9fc3
growlf <growlf@oddotter.com>
2015-07-26 11:14:06 -0700

    Added angular router

---
+++ client/scss/site/_footer.scss
@@ -0,0 +1,11 @@
+angular.module("yetibox").config(['$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+            .state('home', {
+                url: "/",
+                template: UiRouter.template('home.html')
+            });
+    }
+]);
