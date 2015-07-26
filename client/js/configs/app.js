f525f28c5f8bed4a59c5839e3113559acc5bdcae
growlf <growlf@oddotter.com>
2015-07-26 14:34:14 -0700

    Trying to implement orion pages.. WARNING: this is not working

--- client/js/configs/app.js
+++ client/js/configs/app.js
@@ -2,4 +2,9 @@ if (Meteor.isClient) {
 
   angular.module('yetibox', ['angular-meteor', 'ui.router']);
 
+  orion.dictionary.addDefinition('description', 'site', {
+      type: String,
+      label: 'Description'
+  });
+  
 }
--- server/js/configs/app.js
+++ server/js/configs/app.js
@@ -1,1 +1,14 @@
-//if (Meteor.if(Meteor.isServer){ })
+Meteor.if(Meteor.isServer)(
+
+  orion.pages.addTemplate({
+    layout: ReactiveTemplates.get('layout'),
+    template: 'pagesSimple',
+    name: 'Simple',
+    description: 'Simple template'
+  }, {
+    content: orion.attribute('froala', {
+      label: 'Content'
+    })
+  })
+
+);
