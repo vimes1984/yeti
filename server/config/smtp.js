86eccc6908cb686b977774f2332fd4313ae5ca57
vimes1984 <churchill.c.j@gmail.com>
2015-07-31 21:35:42 +0200

    gitignore

--- server/config/smtp.js
+++ server/config/smtp.js
@@ -1,13 +1,11 @@
 //
-/*
 Meteor.startup(function () {
   smtp = {
-    username: 'your_username',   // eg: server@gentlenode.com
-    password: 'your_password',   // eg: 3eeP1gtizk5eziohfervU
-    server:   'smtp.gmail.com',  // eg: mail.gandi.net
-    port: 25
+    username: 'test@oddotter.com',
+    password: 'S1mpl3##',
+    server:   'smtp.gmail.com',
+    port: 465
   }
 
   process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
 });
-*/
