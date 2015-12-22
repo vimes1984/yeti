28f866f7e4ec901c4e98d831c118ec8da940e4bb
vimes1984 <churchill.c.j@gmail.com>
2015-12-22 21:46:44 +0100

    addingin minor change

--- server/methods.js
+++ server/methods.js
@@ -5,6 +5,7 @@
 Meteor.methods({
     saveToFile: function(arg){
         console.log(arg);
+        var fileSave = Meteor.npmRequire("file-save");
         var file = fileSave('test.js');
     }
 });
