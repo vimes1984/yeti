bfdd0ec3ff8c734fadd28cd9a02ce9b23ebf54eb
growlf <growlf@oddotter.com>
2015-12-22 12:57:38 -0800

    Adding file save functionality

--- server/methods.js
+++ server/methods.js
@@ -6,6 +6,28 @@ Meteor.methods({
     saveToFile: function(arg){
         console.log(arg);
         var fileSave = Meteor.npmRequire("file-save");
-        var file = fileSave('test.js');
+    //    var file = fileSave('xyzzy.js');
+    //    file.write(arg, 'utf8');
+    //    file.end();
+
+
+        fileSave('xyzzy.js')
+            .write('this is the first line', 'utf8')
+            .write('this is the second line', 'utf8', function() {
+                console.log('writer callback')
+            })
+            .end('this is the end')
+            .error(function() {
+                console.log('error goes here')
+            })
+            .finish(function() {
+                console.log('write finished.')
+            })
+
     }
+
+
+
 });
+
+
