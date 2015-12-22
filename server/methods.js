11e52f1c17edfe999a49d91bf51c72da8584d758
growlf <growlf@oddotter.com>
2015-12-22 13:36:59 -0800

    Testing slack commit alerts

--- server/methods.js
+++ server/methods.js
@@ -6,24 +6,11 @@ var fs = Meteor.npmRequire("fs");
 Meteor.methods({
     saveToFile: function(arg){
         console.log(arg);
-        fs.writeFile('message.txt', 'Hello Node.js', function (err) {
+        fs.writeFile('./xyzzy.js', arg, function (err) {
           if (err) throw err;
           console.log('It\'s saved!');
         });
         return fs;
-        /*fileSave('xyzzy.js')
-            .write('this is the first line', 'utf8')
-            .write('this is the second line', 'utf8', function() {
-                console.log('writer callback');
-            })
-            .end('this is the end')
-            .error(function() {
-                console.log('error goes here');
-            })
-            .finish(function() {
-                console.log('write finished.');
-            });
-*/
     }
 
 });
