a170948813d2050b45a9a4ecfedc9df285e0e10c
vimes1984 <churchill.c.j@gmail.com>
2015-12-22 22:00:04 +0100

    addingin minor change

--- server/methods.js
+++ server/methods.js
@@ -14,20 +14,18 @@ Meteor.methods({
         fileSave('xyzzy.js')
             .write('this is the first line', 'utf8')
             .write('this is the second line', 'utf8', function() {
-                console.log('writer callback')
+                console.log('writer callback');
             })
             .end('this is the end')
             .error(function() {
-                console.log('error goes here')
+                console.log('error goes here');
             })
             .finish(function() {
-                console.log('write finished.')
-            })
+                console.log('write finished.');
+            });
 
     }
 
 
 
 });
-
-
